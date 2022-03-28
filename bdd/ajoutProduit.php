79bcdef179a8ab080dfb8814d62628035de6d58e
Hafarou <hafaroupro@gmail.com>
2022-03-28 14:58:55 +0200

    Update fichiers

--- bdd/ajoutProduit.php
+++ bdd/ajoutProduit.php
@@ -13,11 +13,23 @@
         <input type="text" name="nom_produit">
         <p>Saisir le contenu du produit :</p>
         <textarea type="text" name="contenu_produit" cols="40" rows="5"></textarea>
-        <p><input type="submit" value="Save"></p>
+        <p><input type="submit" value="Ajouter"></p>
     </form>
+    
     <?php
+        // Pour inclure le fichier de connexion à la bdd 
         include "connexionBDD.php";
+        // Pour inclure le ficiers fonctions
         include "fonctions.php";
+        if(isset($_POST['nom_produit']) && isset($_POST['contenu_produit']) && ($_POST['nom_produit'] != "") && ($_POST['contenu_produit'] != "")){
+            $nom = $_POST['nom_produit'];
+            $contenu = $_POST['contenu_produit'];
+            insertProduit($bdd, $nom, $contenu);
+            echo "<p>$nom à été ajouter en BDD</p>"; 
+        }
+        else{
+            echo "<p>Veuillez remplir les champs du formulaire</p>";
+        }
     ?>
 </body>
 </html>
--- bdd/fonctions.php
+++ bdd/fonctions.php
@@ -1,4 +1,19 @@
 <?php 
-    
+    // fonction d'ajout d'un produit
+    function insertProduit($bdd, $nom, $contenu){
+        try{
+            $req = $bdd->prepare('INSERT INTO produit(nom_produit, contenu_produit) 
+            VALUES(:nom_produit, :contenu_produit);');
+            $req->execute(array(
+                'nom_produit' => $nom,
+                'contenu_produit' =>$contenu
+            ));
+
+        }
+        catch(Exception $e){
+            // affichage d'une exception en cas d'ereur
+            die('Erreur '.$e->getMessage());
+        }
+    }
 
 ?>
